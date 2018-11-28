0d6e908ac647008f486cbc5c16f03d3cd7727467
Andrew Olson <ajolson7@gmail.com>
2018-11-27 18:11:19 -0700

    Created a starter modal for adding a new type of item to track. Also created a table that displays each trackable item, its total lot count, and the earliest expiration of its lots.

--- src/App.js
+++ src/App.js
@@ -6,17 +6,51 @@ class App extends Component {
     super();
     console.log(this);
     this.state = {
-      trackables : {
-        A: 15,
-        B: 400,
-      }
+      trackables : [
+        {
+          name: 'morty',
+          perishable: true,
+          yourmom: true,
+          lots: [
+            {
+              count: 40,
+              creation_date: '2018-10-30T16:23:16Z',
+              expiration_date: '2018-11-29T06:00:00Z'
+            },
+            {
+              count: 70,
+              creation_date: '2018-10-30T14:23:16Z',
+              expiration_date: '2018-11-30T07:00:00Z'
+            }
+          ]
+        },
+        {
+          name: 'bananas',
+          perishable: true,
+          yourmom: true,
+          lots: [
+            {
+              count: 100,
+              creation_date: '2018-09-15T10:24:19Z',
+              expiration_date: '2018-12-25T04:00:00Z'
+            },
+            {
+              count: 30,
+              creation_date: '2018-08-04T08:13:09Z',
+              expiration_date: '2018-09-04T09:00:00Z'
+            }
+          ]
+        }
+      ]
     }
+    // this.newTrackable = this.newTrackable.bind(this);
   }
 
-  newTrackable(e) {
+  newTrackable = (e) => {
     console.log(e);
     // TODO
-
+    this.setState({ trackables: { ...this.state.trackables, C: 2000000000000000000 } });
+    this.refs.addTrackable.showModal();
   }
 
   render() {
@@ -31,8 +65,43 @@ class App extends Component {
         </nav>
         {/* routed main OR inventory screen if we're gonna do modals for everything */}
         <div>
-          {Object.keys(this.state.trackables).map(trackableKey => { return <p>{this.state.trackables[trackableKey]}</p>})}
+          <table>
+            <thead>
+              <tr>
+                <th>Trackable</th>
+                <th>Total Inventory</th>
+                <th>Earliest Lot Expiration</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.trackables.map(trackable => { 
+                return <tr>
+                  <td>{trackable.name}</td>
+                  <td>{trackable.lots.reduce((sum, curr) => {
+                    return sum + curr.count
+                  }, 0
+                  )}</td>
+                  <td>{new Date(trackable.lots.reduce((earliestExp, curr) => {
+                    let newestExp = (new Date (curr.expiration_date)).valueOf();
+                    if (earliestExp > newestExp || !earliestExp) {
+                      return newestExp;
+                    }
+                    return earliestExp;
+                  }, null
+                  )).toDateString()}</td>
+                </tr>
+              })}
+            </tbody>
+          </table>
         </div>
+
+        <dialog id="addTrackable" ref="addTrackable">
+          <form method="dialog">
+            <p><label>LABEL BABY</label></p>
+            <button>Confirm</button>
+            <button>Cancel</button>
+          </form>
+        </dialog>
       </div>
     );
   }
