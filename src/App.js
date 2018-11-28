35251c02fe44c2120a85d01f16bf485c9f2ad721
Andrew Olson <ajolson7@gmail.com>
2018-11-27 21:29:11 -0700

    Did a whole bunch of stuff; the app can now be used to add trackables, increment inventory for already existing trackables, and decrement inventory from already existing trackables. Pretty neat.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ class App extends Component {
     this.state = {
       addTrackableName: '',
       addTrackablePerishable: false,
+      addInventoryCount: 0,
+      addInventoryPerishableDate: '',
+      addInventoryTrackableType: 'morty',
+      decrementInventoryTrackableType: 'morty',
+      decrementInventoryCount: 0,
       trackables : [
         {
           name: 'morty',
@@ -17,12 +22,12 @@ class App extends Component {
             {
               count: 40,
               creation_date: '2018-10-30T16:23:16Z',
-              expiration_date: '2018-11-29T06:00:00Z'
+              expiration_date: '2018-11-29'
             },
             {
               count: 70,
               creation_date: '2018-10-30T14:23:16Z',
-              expiration_date: '2018-11-30T07:00:00Z'
+              expiration_date: '2018-11-30'
             }
           ]
         },
@@ -55,12 +60,109 @@ class App extends Component {
     this.refs.addTrackable.showModal();
   }
 
+  receiveShipment = () => {
+    this.refs.addInventory.showModal();
+  }
+
+  removeInventory = () => {
+    this.refs.decrementInventory.showModal();
+  }
+
+  decrementInventoryCountOnChange = (e) => {
+    this.setState({ decrementInventoryCount: parseInt(e.target.value) })
+  }
+
+  decrementInventoryTrackableTypeOnChange = (e) => {
+    this.setState({ decrementInventoryCount: e.target.value });
+  }
+
+  addInventoryCountOnChanage = (e) => {
+    this.setState({ addInventoryCount: parseInt(e.target.value) });
+  }
+
+  addInventoryTrackableTypeOnChange = (e) => {
+    this.setState({ addInventoryTrackableType: e.target.value });
+  }
+
+  addInventoryPerishableDateOnChange = (e) => {
+    this.setState({ addInventoryPerishableDate: e.target.value });
+  }
+
   addTrackableNameOnChange = (e) => {
     this.setState({addTrackableName: e.target.value});
   }
 
   addTrackablePerishableOnChange = (e) => {
     this.setState({addTrackablePerishable: e.target.checked});
+  }
+
+  decrementInventorySave = () => {
+    console.log('neals mom');
+    let trackable = this.state.trackables.find(tr => {
+      return tr.name === this.state.addInventoryTrackableType;
+    });
+
+    if (this.state.decrementInventoryCount < 1 || !trackable || this.state.decrementInventoryCount > trackable.lots.reduce((sum, curr) => {
+      return sum + curr.count;
+    }, 0
+    )) {
+      alert("Not a valid amount");
+      this.decrementInventoryClear();
+      return;
+    }
+    
+    let sortedLots = trackable.lots.sort((lot1, lot2) => {
+      if (lot1.expiration_date < lot2.expiration_date) {
+        return -1;
+      }
+      return 1;
+    });
+
+    while (this.decrementInventoryCount > 0) {
+      let currentLot = sortedLots.shift();
+
+      if (currentLot.count > this.decrementInventoryCount) {
+        currentLot.count -= this.decrementInventoryCount;
+        this.setState({ decrementInventoryCount: 0 });
+      } else {
+        this.setState({ decrementInventoryCount: this.state.decrementInventoryCount - currentLot.count });
+        let index = trackable.lots.findIndex(lot => {
+          return lot.creation_date === currentLot.creation_date;
+        });
+        trackable.lots.splice(index, 1);
+      }
+    }
+  }
+
+  decrementInventoryClear = () => {
+    this.setState({ decrementInventoryCount: 0, decrementInventoryTrackableType: 'morty' });
+  }
+
+  addInventorySave = () => {
+    let trackable = this.state.trackables.find(tr => {
+      return tr.name === this.state.addInventoryTrackableType;
+    });
+
+    if (this.state.addInventoryCount < 1 || !trackable) {
+      alert("Not a valid amount");
+      this.addInventoryClear();
+      return;
+    }
+
+    trackable.lots.push({ 
+      count: this.state.addInventoryCount,
+      creation_date: (new Date()).toISOString(),
+      expiration_date: this.state.addInventoryPerishableDate
+     });
+
+     this.addInventoryClear();
+  }
+
+  addInventoryClear = () => {
+    this.setState({ addInventoryCount: 0,
+      addInventoryPerishableDate: '',
+      addInventoryTrackableType: 'morty'
+    });
   }
 
   addTrackableSave = () => {
@@ -130,13 +232,45 @@ class App extends Component {
           </table>
         </div>
 
+        {/* Add new trackable form dialog */}
         <dialog id="addTrackable" ref="addTrackable">
           <form method="dialog">
-            <p><label>LABEL BABY</label></p>
+            <p><label>Add New Trackable</label></p>
             <input type="text" placeholder="name" value={this.state.addTrackableName} onChange={this.addTrackableNameOnChange} />
             <input type="checkbox" checked={this.state.addTrackablePerishable} onChange={this.addTrackablePerishableOnChange} />
             <button onClick={this.addTrackableSave}>Confirm</button>
             <button onClick={this.addTrackableClear}>Cancel</button>
+          </form>
+        </dialog>
+
+        {/* Add inventory form dialog */}
+        <dialog id="addInventory" ref="addInventory">
+          <form method="dialog">
+            <p><label>Add Inventory</label></p>
+            <select id="trackableId" value={this.state.addInventoryTrackableType} onChange={this.addInventoryTrackableTypeOnChange}>
+              {this.state.trackables.map(trackable => {
+                return <option value={trackable.name}>{trackable.name}</option>
+              })}
+            </select>
+            <input type="number" placeholder="count" value={this.state.addInventoryCount} onChange={this.addInventoryCountOnChanage} />
+            <input type="date" value={this.state.addInventoryPerishableDate} onChange={this.addInventoryPerishableDateOnChange} />
+            <button onClick={this.addInventorySave}>Confirm</button>
+            <button onClick={this.addInventoryClear}>Cancel</button>
+          </form>
+        </dialog>
+
+        {/* Decrement inventory form dialog */}
+        <dialog id="decrementInventory" ref="decrementInventory">
+          <form method="dialog">
+            <p><label>Decrement Inventory</label></p>
+            <select id="trackableId" value={this.state.decrementInventoryTrackableType} onChange={this.decrementInventoryTrackableTypeOnChange}>
+              {this.state.trackables.map(trackable => {
+                return <option value={trackable.name}>{trackable.name}</option>
+              })}
+            </select>
+            <input type="number" placeholder="count" value={this.state.decrementInventoryCount} onChange={this.decrementInventoryCountOnChange} />
+            <button onClick={this.decrementInventorySave}>Confirm</button>
+            <button onClick={this.decrementInventoryClear}>Cancel</button>
           </form>
         </dialog>
       </div>
